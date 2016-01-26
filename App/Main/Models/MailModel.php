1e62f41fdaf720fd883e600a64872cff4af2c352
mustafa220 <musto_220@windowslive.com>
2016-01-26 14:35:41 +0200

    Changes

---
+++ App/Main/Models/MailModel.php
@@ -0,0 +1,35 @@
+<?php
+	namespace Models;
+	use MS\MSModel;
+	use PHPMailer;
+	class MailModel extends MSModel{
+		function __construct(){
+			parent::__construct();
+			
+		}
+		function veriler(){
+			$mail = new PHPMailer();
+			$mail->IsSMTP();
+			$mail->SMTPDebug = 2;
+			$mail->DebugOutput = "html";
+			$mail->host = "mail.mcolak.net";
+			$mail->Port = 587;
+			$mail->SMTPSecure = "tls";
+			$mail->SMTPAuth = true;
+			$mail->Username = "mustafac@mcolak.net";
+			$mail->Password = "a05466329428a";
+			$mail->SetFrom("mustafac@mcolak.net","Mustafa Ç.");
+			$mail->AddAddress("musto_220@windowslive.com","Gönderilen?");
+			$mail->Subject = "test maili";
+			$mail->msgHTML('<b>Test maili geliyor :D </b>');
+			if($mail->send()){
+				echo '<font color=green>mail gönderildi</font>';
+			}
+			else{
+				echo "<font color=red>mail gönderilemedi</font><br/>";
+				echo "<b>".$mail->ErrorInfo."</b>";
+			}
+			
+		}
+	}
+?>
